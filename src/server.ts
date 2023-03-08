bf27fc9a856cc1bceb236a82cf0c8b1c2001fce4
Thiago Silva <itm_tsilva@uolinc.com>
2023-03-08 10:57:43 -0300

    feat: registrado o plugin da rota de transactions

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,31 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async (req, res) => {
-  const result = await knex('transactions').select('*')
-  return result
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
-app.get('/hello2', async (req, res) => {
-  const result = await knex('sqlite_schema').select('*')
-  return result
-})
+// app.get('/hello2', async (req, res) => {
+//   const result = await knex('sqlite_schema').select('*')
+//   return result
+// })
 
-app.get('/hello3', async (req, res) => {
-  const result = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transacao de teste',
-      amount: 1000,
-    })
-    .returning('*')
-  return result
-})
+// app.get('/hello3', async (req, res) => {
+//   const result = await knex('transactions')
+//     .insert({
+//       id: crypto.randomUUID(),
+//       title: 'Transacao de teste',
+//       amount: 1000,
+//     })
+//     .returning('*')
+//   return result
+// })
 
 app
   .listen({
-    port: 8878,
+    port: env.PORT,
   })
   .then(() => console.log('Http server listening'))
