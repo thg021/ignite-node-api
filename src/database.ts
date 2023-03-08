c970b368695007f52f2d3df1846c5259dab82e3e
Thiago Silva <itm_tsilva@uolinc.com>
2023-03-08 10:56:47 -0300

    feat: adicionado  as variveis no env

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,11 @@
 import { knex as setupKnex, Knex } from 'knex'
 import 'dotenv/config'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('Database URL must be specified')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
