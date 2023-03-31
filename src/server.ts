76e7c42a9ceb1fd4ed4316a1c0931069bc2aee97
thiago da silva <thg021@gmail.com>
2023-03-31 08:17:49 -0300

    refactor: separacao em app e server para realizacao de testes e2e

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-// app.get('/hello2', async (req, res) => {
-//   const result = await knex('sqlite_schema').select('*')
-//   return result
-// })
-
-// app.get('/hello3', async (req, res) => {
-//   const result = await knex('transactions')
-//     .insert({
-//       id: crypto.randomUUID(),
-//       title: 'Transacao de teste',
-//       amount: 1000,
-//     })
-//     .returning('*')
-//   return result
-// })
 
 app
   .listen({
