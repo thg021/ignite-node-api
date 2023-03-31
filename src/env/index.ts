444c83dd138fc3ff0be959d9c4263f06a83d05a8
thiago da silva <thg021@gmail.com>
2023-03-31 08:17:20 -0300

    feat: validacao de ambiente de teste para criacao de um banco de dados somente para testes

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,6 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test', override: true })
+} else {
+  config()
+}
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_URL: z.string(),
